0e225b4dce26e6d4d677812c81d9cb5ad1b0a09b
poweronio <team@poweron.io>
2015-11-02 14:35:25 -0500

    math fix

--- src/js/controllers/form.js
+++ src/js/controllers/form.js
@@ -276,415 +276,416 @@ app.controller('FormDemoCtrl2', ['$http', '$scope', 'GoogleDistanceAPI', '$modal
             "id": 0,
             "item": "Salads",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 1,
             "item": "House Salad W/ Dressing",
             "price": 21.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 2,
             "item": "Caesar Salad",
             "price": 24.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 3,
             "item": "Garlic Bread (3 Dozen)",
             "price": 21.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 4,
             "item": "Pasta",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 5,
             "item": "Pasta W/ Meatballs",
             "price": 49.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 6,
             "item": "Lasagna (12 Pcs.)",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 7,
             "item": "Baked Ziti",
             "price": 59.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 8,
             "item": "Rigatoni Bolognese",
             "price": 59.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 9,
             "item": "Penne Ala Vodka",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 10,
             "item": "Raviolis",
             "price": 49.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 11,
             "item": "Chicken",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 12,
             "item": "Penne W/ Chicken Broccoli Creamy Garlic",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 13,
             "item": "Chicken Scarpariello W/ Linguine",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 14,
             "item": "Chicken Parmigiana",
             "price": 69.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 15,
             "item": "Chicken Francese",
             "price": 74.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 16,
             "item": "Chicken Marsala",
             "price": 74.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 17,
             "item": "Chicken Picatta",
             "price": 69.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 18,
             "item": "Chicken Cacciatore W/ Penne",
             "price": 69.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 19,
             "item": "Vegetarian",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 20,
             "item": "Penne Primavera",
             "price": 49.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 21,
             "item": "Vegetable Lasagna (12Pcs.)*",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 22,
             "item": "Broccoli, Garlic & Oil W/ Rigatoni",
             "price": 54.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 23,
             "item": "Eggplant Rollatini",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 24,
             "item": "Eggplant Parmigiana",
             "price": 64.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 25,
             "item": "Seafood",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 26,
             "item": "Snapper Oreganato",
             "price": 95.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 27,
             "item": "Tilapia Francese",
             "price": 79.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 28,
             "item": "Shrimp Scampi W/ Linguini",
             "price": 84.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 29,
             "item": "Shrimp Parmigiana*",
             "price": 84.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 30,
             "item": "Stuffed Shrimp*",
             "price": 94.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 31,
             "item": "Frutti Di Mare W/ Linguine",
             "price": 95.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 32,
             "item": "Assorted Sushi (Chefs Choice)\nSushi Before 5Pm Please Call",
             "price": 1.25,
-            "note": "50(minimum)"
+            "note": "50(minimum)",
+            "quantity":0
   },
         {
             "id": 33,
             "item": "Meats",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 34,
             "item": "Meaty Macaroni*",
             "price": 59.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 35,
             "item": "Meatballs (Half Tray â€“ 40 Pcs.)",
             "price": 49.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 36,
             "item": "Sausage & Peppers",
             "price": 52.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 37,
             "item": "Grilled Stuff Skirt Steak (12 Pcs.)",
             "price": 84.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 38,
             "item": "Filet Mignon Courvoisier",
             "price": 195,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 39,
             "item": "Sides",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 40,
             "item": "Cold Antipasto",
             "price": 59.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 41,
             "item": "Roasted Potatoes",
             "price": 39.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 42,
             "item": "Sauteed Vegetables",
             "price": 39.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 43,
             "item": "Hors D'oeuvre/Appetizers",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 44,
             "item": "Meatballs",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 45,
             "item": "Fried Mozzarella",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 46,
             "item": "Fried Mozzarella & Tomato Skewers",
             "price": 1.75,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 47,
             "item": "Stuffed Mushrooms",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 48,
             "item": "Rice Balls",
             "price": 1.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 49,
             "item": "Spinach Rolls",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 50,
             "item": "Stromboli",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 51,
             "item": "Rabe & Sausage Balls",
             "price": 1.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 52,
             "item": "Sausage, Pepper & Onion Skewers",
             "price": 1.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 53,
             "item": "Mini Ravioli & Mozzarella Skewers",
             "price": 1.75,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 54,
             "item": "Potato Croquettes",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 55,
             "item": "Beef Wellington",
             "price": 3.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 56,
             "item": "Bacon Wrapped Scallops",
             "price": 3.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 57,
             "item": "Shrimp Cocktail",
             "price": 2.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 58,
             "item": "Jumbo Lump Crab Cake",
             "price": 2.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 59,
             "item": "Assorted Sushi (Chefs Choice)",
             "price": 1.25,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 60,
             "item": "Sesame Chicken Skewers",
             "price": 2,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 61,
             "item": "Desserts",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 62,
             "item": "Assorted Dessert Platter\nChefs Choice (10Pcs.)",
             "price": 49.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 63,
             "item": "Homemade Cookies (1 Tray)",
             "price": 18.95,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 64,
             "item": "Setup Charges",
             "price": null,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 65,
             "item": "Chaffer Dish Rental",
             "price": 10,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 66,
             "item": "Delivery Charge",
             "price": 15,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 67,
             "item": "Plastic Ware",
             "price": 0.5,
-            "note": ""
+            "quantity":0
   },
         {
             "id": 68,
             "item": "Setup & Pick-Up Fee",
             "price": 50,
-            "note": ""
+            "quantity":0
   }
 ];
 
@@ -698,10 +699,23 @@ app.controller('FormDemoCtrl2', ['$http', '$scope', 'GoogleDistanceAPI', '$modal
     }
 
     $scope.change = function (key) {
-        var subtotal = $scope.thing.price * key;
+        console.log(key);
+        console.log($scope.products[key]);
+        //console.log($scope.item.quantity);
+        //$scope.products[key].
+        //$scope.products[key].quantity = $scope.item.quantity;
+        var subtotal =0;
+        for(var i=0; i<$scope.products.length;i++){
+            if($scope.products[i].price){
+            subtotal += $scope.products[i].price * $scope.products[i].quantity;
+            console.log(i + ':- ' + $scope.products[i].item + subtotal);
+            }
+        }
+         //var subtotal = $scope.products[key].price * $scope.products[key].quantity;
+        //$scope.subtotal = subtotal;
         $scope.subtotal = subtotal;
         $scope.tax = subtotal * 0.06;
-        $scope.total = $scope.delivery + $scope.tax + $scope.thing.price * $scope.multiplier;
+        $scope.total = $scope.delivery + $scope.tax + $scope.subtotal;
     }
 
     $scope.result = ''
